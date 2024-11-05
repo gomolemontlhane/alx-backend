8e7b12d58939c3bc20cd8f7549a532c301b4c64b
gomolemontlhane <gomolemontlhane@gmail.com>
2024-11-05 19:37:33 +0200

    fixing pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,21 @@
-# 0-app.py
+#!/usr/bin/env python3
+"""
+0-app.py: A simple Flask application that renders an HTML page.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> str:
+    """
+    Renders the index page with a welcome message.
+
+    Returns:
+        str: The rendered HTML template.
+    """
     return render_template('0-index.html')
 
 
