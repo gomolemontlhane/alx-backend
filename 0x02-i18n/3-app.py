0e7c9f2e7df9ee327fabba76266a5a765a1c09ba
gomolemontlhane <gomolemontlhane@gmail.com>
2024-11-05 23:43:31 +0200

    fixing errors

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,24 +1,17 @@
 #!/usr/bin/env python3
-"""
-Flask app
-"""
-from flask import (
-    Flask,
-    render_template,
-    request
-)
-from flask_babel import Babel
+"""basic flask application"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
 
 
-class Config(object):
-    """
-    Configuration for Babel
-    """
+class Config:
+    """ Application configuration class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+# Instantiate the application object
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
@@ -26,19 +19,15 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """
-    Select and return best language match based on supported languages
-    """
+    """ets locale from request object"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Handles / route
-    """
+@app.route('/')
+def index():
+    """Renders a basic html template"""
     return render_template('3-index.html')
 
 
-if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+    app.run()
